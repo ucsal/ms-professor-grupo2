8c3449771896f28267505c78f4ae1bf1372f4158
Tailan Santos <tailansanttos02@gmail.com>
2026-06-05 16:14:37 -0300

    fix

--- src/main/java/com/ms_professor/client/AuthClient.java
+++ src/main/java/com/ms_professor/client/AuthClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "ms-auth", url = "http://localhost:8080/api/auth")
+@FeignClient(name = "ms-auth", url = "http://localhost:8089/api/auth")
 public interface AuthClient {
     @PostMapping("/register/professor")
     void cadastrarProfessor(@RequestBody UsuarioRegisterDTO usuarioRegisterDTO);
